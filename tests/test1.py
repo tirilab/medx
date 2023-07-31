eb26c381cbe73306f225b3cc1e684be732c8937a
Louise Lu <59856378+L0U153@users.noreply.github.com>
2023-07-31 15:59:56 -0700

    Add files via upload

---
+++ tests/test1.py
@@ -0,0 +1,13 @@
+import unittest
+import medx
+
+class TestMrci(unittest.TestCase):
+    def test_single(self):
+        self.assertEqual(medx.mrciCalc('../../sample_data/sample_med.csv', '../../test/sample_out.csv'), 1)
+    
+    def test_compare(self):
+        self.assertEqual(medx.mrciCompa('../../sample_data/sample_med.csv', '../../test/sample_out2.csv'), 1)
+        
+
+if __name__ == '__main__':
+    unittest.main()
